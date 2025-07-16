024bcf1647bdf4824b3033e377be16442f9a93fb
suzusan555 <ryuusei.8hatake@gmail.com>
2025-07-16 11:44:48 +0900

    コントローラファイル作成

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class Controller extends BaseController
+{
+    public function index();
+    
+}
